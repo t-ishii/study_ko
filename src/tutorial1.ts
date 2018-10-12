9e8efe71f5faee0d8682de7457451d0e8a8cd9d9
tatsuya ishii <perlispython.plp@gmail.com>
2018-10-13 00:56:12 +0900

    Finish tutorial introduction

--- src/tutorial1.ts
+++ src/tutorial1.ts
@@ -1,11 +1,17 @@
 import ko from './lib/knockout-3.4.2';
 
 class AppViewModel {
-    firstName: string;
-    lastName: string;
+    firstName: (string?) => string;
+    lastName: (string?) => string;
+    fullName: (string?) => string;
+    capitalizeLastName: () => void;
     constructor() {
-        this.firstName = 'Bert';
-        this.lastName = 'Bertington';
+        this.firstName = ko.observable('Bert');
+        this.lastName = ko.observable('Bertington');
+        this.fullName = ko.computed((): string => `${this.firstName()} ${this.lastName()}`);
+        this.capitalizeLastName = (): void => {
+            this.lastName(this.lastName().toUpperCase());
+        }
     }
 }
 
