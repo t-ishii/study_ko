a8f6e41075aa38960a66f07e07bf316ba82fe243
tatsuya ishii <perlispython.plp@gmail.com>
2018-10-12 14:29:40 +0900

    Modify build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+const getEntryFiles = () => {
+  const fileMap = {}
+  const tsFiles = fs.readdirSync('./src').filter((file) => /\.ts$/.test(file))
+  tsFiles.forEach(file => {
+    fileMap[file] = path.resolve(__dirname, path.join('src', file))
+  });
+  return fileMap
+}
+
 module.exports = {
   mode: 'development',
-  entry: './src/main.ts',
+  entry: getEntryFiles(),
+  output: {
+    filename: '[name].js'
+  },
   module: {
     rules: [
       {
@@ -11,7 +26,8 @@ module.exports = {
   },
   resolve: {
     extensions: [
-      '.ts'
+      '.ts',
+      '.js'
     ]
   }
-};
+}
