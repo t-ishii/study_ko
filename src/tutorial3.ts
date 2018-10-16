003caff0dc80b83ebc52f8c6952fde84e1d4f46d
tatsuya ishii <perlispython.plp@gmail.com>
2018-10-16 21:11:23 +0900

    Finish step 3

--- src/tutorial3.ts
+++ src/tutorial3.ts
@@ -3,45 +3,63 @@ import ko from './lib/knockout-3.4.2';
 const mails = {
     Inbox: {
         mails: [
-          {
-            from: "xxx@xxx1",
-            to: "aaa@aaa",
-            subject: "Inbox",
-            date: "2XXX/XX/XX"
-          },
-          {
-            from: "xxx@xxx2",
-            to: "aaa@aaa",
-            subject: "Inbox",
-            date: "2XXX/XX/XX"
-          },
-          {
-            from: "xxx@xxx3",
-            to: "aaa@aaa",
-            subject: "Inbox",
-            date: "2XXX/XX/XX"
-          },
+            {
+                id: 0,
+                from: "xxx@xxx1",
+                to: "aaa@aaa",
+                subject: "Inbox",
+                date: "2XXX/XX/XX",
+                folder: 'Inbox',
+                messageContent: 'Content'
+            },
+            {
+                id: 1,
+                from: "xxx@xxx2",
+                to: "aaa@aaa",
+                subject: "Inbox",
+                date: "2XXX/XX/XX",
+                folder: 'Inbox',
+                messageContent: 'Content'
+            },
+            {
+                id: 2,
+                from: "xxx@xxx3",
+                to: "aaa@aaa",
+                subject: "Inbox",
+                date: "2XXX/XX/XX",
+                folder: 'Inbox',
+                messageContent: 'Content'
+            },
         ]
     },
     Archive: {
         mails: [
           {
+            id: 0,
             from: "xxx@xxx1",
             to: "aaa@aaa",
             subject: "Archive",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Archive',
+            messageContent: 'Content'
           },
           {
+            id: 1,
             from: "xxx@xxx2",
             to: "aaa@aaa",
             subject: "Archive",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Archive',
+            messageContent: 'Content'
           },
           {
+            id: 2,
             from: "xxx@xxx3",
             to: "aaa@aaa",
             subject: "Archive",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Archive',
+            messageContent: 'Content'
           },
         ]
 
@@ -49,44 +67,62 @@ const mails = {
     Sent: {
         mails: [
           {
+            id: 0,
             from: "xxx@xxx1",
             to: "aaa@aaa",
             subject: "Sent",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Sent',
+            messageContent: 'Content'
           },
           {
+            id: 1,
             from: "xxx@xxx2",
             to: "aaa@aaa",
             subject: "Sent",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Sent',
+            messageContent: 'Content'
           },
           {
+            id: 2,
             from: "xxx@xxx3",
             to: "aaa@aaa",
             subject: "Sent",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Sent',
+            messageContent: 'Content'
           },
         ]
     },
     Spam: {
         mails: [
           {
+            id: 0,
             from: "xxx@xxx1",
             to: "aaa@aaa",
             subject: "Spam",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Spam',
+            messageContent: 'Content'
           },
           {
+            id: 1,
             from: "xxx@xxx2",
             to: "aaa@aaa",
             subject: "Spam",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Spam',
+            messageContent: 'Content'
           },
           {
+            id: 2,
             from: "xxx@xxx3",
             to: "aaa@aaa",
             subject: "Spam",
-            date: "2XXX/XX/XX"
+            date: "2XXX/XX/XX",
+            folder: 'Spam',
+            messageContent: 'Content'
           },
         ]
     }
@@ -101,20 +137,32 @@ class WebmailViewModel {
     folders: string[];
     chosenFolderId: (string) => void;
     chosenFolderData: any;
+    chosenMailData: any;
 
     // Behaviours
     goToFolder: (string) => void;
+    goToMail: (string) => void;
+
     constructor () {
         this.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
         this.chosenFolderId = ko.observable();
         this.chosenFolderData = ko.observable();
+        this.chosenMailData = ko.observable();
 
         this.goToFolder = (folder: string) => {
             this.chosenFolderId(folder);
+            this.chosenMailData(null);
             getMails(folder).then((res) => {
                 this.chosenFolderData(res);
-            })
+            });
         };
+        this.goToMail = (mail: any) => {
+            this.chosenFolderId(mail.folder);
+            this.chosenFolderData(null);
+            console.log(mail);
+            this.chosenMailData(mails[mail.folder].mails.find(d => d.id === mail.id));
+        };
+
         this.goToFolder('Inbox');
     }
 }
